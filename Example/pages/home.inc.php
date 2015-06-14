05a85ee9072a520befe3c4c2298e9cdc854049ae
SkyzohKey <skyzohkey@gmail.com>
2015-06-14 21:27:05 +0200

    Reworked lang processing

--- Example/pages/home.inc.php
+++ Example/pages/home.inc.php
@@ -1,4 +1,4 @@
-<div style="margin: 150px auto; width: 800; padding: 20px; background: #ddd; color:#1E90FF; border-radius: 3px;">
+<div>
     <h1>{website.welcome}</h1>
     <h2>{website.tagline}</h2>
     <p>{website.home.introduction}</p>
